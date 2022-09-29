1b8148143e9199e9dda6d99952d6ccaa75ad7087
Dongwon Kang <dw3624@gmail.com>
2022-09-29 19:24:37 +0900

    update: v1.0.2

--- README.md
+++ README.md
@@ -12,16 +12,16 @@
 ## manifest.json
 ```json
 {
-  "name": "Naver News Filter",
-  "short_name": "nnf",
-  "version": "1.0.0",
+  "name": "네이버 포토뉴스 필터",
+  "short_name": "naver photo news filter",
+  "version": "1.0.2",
   "manifest_version": 3,
   "icons": {
     "16": "asset/icon16.png",
     "48": "asset/icon48.png",
     "128": "asset/icon128.png"
   },
-  "description": "naver 뉴스 검색에서 포토뉴스를 숨기고 단독뉴스를 강조합니다.",
+  "description": "네이버 뉴스 검색에서 포토뉴스를 숨기고 단독뉴스를 강조합니다.",
   "content_scripts": [{
     "matches": ["https://search.naver.com/search.naver?*where=news*"],
     "js": [
@@ -49,20 +49,37 @@ const checkDandok = (title) => {
 기사 제목에서 `[`와 `]` 사이 문자열을 추출, 사전에 지정한 키워드가 포함된 경우 해당 기사 투명하게 변경합니다.
 ```javascript
 const checkArticle = (news, title, url) => {
-  const titleHead = title.text.match(/\[[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|a-z|A-Z|-]*\]/);
+  const titleHead = title.text.match(/\[[\S|\s]*\]/);
   if (titleHead) {
     for (let i = 0; i < del_titles.length; i++) {
       if (titleHead[0].includes(del_titles[i])) {
-        news.style.opacity = 0.5;
+        news.style.opacity = FILTER_STYLE;
         return;
       };
     };
   }
   for (let j = 0; j < photo_urls.length; j++) {
     if (url.href.includes(photo_urls[j])) {
-      news.style.opacity = 0.5;
+      news.style.opacity = FILTER_STYLE;
       return;
     }
+  };
+};
+```
+
+### checkPress
+- 기능: 기타 특정 언론사 포토뉴스를 필터링합니다.
+- 설명: 언론사별 규칙에 맞춰 코드를 작성합니다.
+```javascript
+const checkPress = (news, press) => {
+  let start = news.querySelector("a.api_txt_lines.dsc_txt_wrap").text[0];
+  if (press === '뉴스핌') {
+    if (start === '=') {
+      news.style.opacity = 0.5;
+      return;
+    };
+  } else {
+    return;
   };
 };
 ```
@@ -70,37 +87,30 @@ const checkArticle = (news, title, url) => {
 ### createKwTag
 a 태그를 만들어 DOM에 추가한 뒤 해당 태그 클릭시 양식에 맞게 변형한 기사 제목이 복사되도록 이벤트를 설정합니다. 추가적으로 style을 지정합니다.
 ```javascript
-const createKwTag = (news, info, title) => {
+const createKwTag = (news, press, title) => {
+  let info = press.replace("언론사 선정", "");
   let pressKw = `${title.text}(${info}, )`;
   let copyKw = document.createElement("a");
   copyKw.innerText = "📋 기사제목(언론사) 복사"
-  copyKw.style = `
-    color: #808080;
-    text-decoration: none;
-    padding-left: 0.8rem;
-  `;
-
+  copyKw.style = styleDefault;
   news.querySelector("div.info_group").appendChild(copyKw);
+
   copyKw.addEventListener("click", () => {
     navigator.clipboard.writeText(pressKw)
   });
+
   copyKw.addEventListener("mouseenter", () => {
-    copyKw.style = `
-      cursor: pointer;
-      color: #808080;
-      text-decoration: underline;
-      padding-left: 0.8rem;
-    `
+    copyKw.style = styleMouseEnter
   });
+
   copyKw.addEventListener("mouseleave", () => {
-    copyKw.style = `
-      color: #808080;
-      text-decoration: none;
-      padding-left: 0.8rem;
-    `
+    copyKw.style = styleDefault;
   });
 }
 ```
+
+
+
 
 # 버전 설명
 ### v1.0.0
@@ -113,3 +123,9 @@ const createKwTag = (news, info, title) => {
 - 기사제목 복사 링크 색, 아이콘 변경
 - 마이너 버그 수정
   - 마우스 호버 시 글씨강조가 마우스 이동 시 해제되도록 수정
+
+### v1.0.2
+- 확장기능 명칭 변경
+  - Naver News Filter → 네이버 포토뉴스 필터
+- 뉴스핌 포토뉴스 필터 기능 추가
+- 일부 코드 리팩토링
--- src/content.js
+++ src/content.js
@@ -1,3 +1,73 @@
+const FILTER_STYLE = 0.5
+
+const checkDandok = (title) => {
+  if (title.text.includes("[단독]")) {
+    title.style = `font-weight: bold;`;
+    title.text = "✅".concat(title.text);
+  }
+};
+
+const checkArticle = (news, title, url) => {
+  const titleHead = title.text.match(/\[[\S|\s]*\]/);
+  if (titleHead) {
+    for (let i = 0; i < del_titles.length; i++) {
+      if (titleHead[0].includes(del_titles[i])) {
+        news.style.opacity = FILTER_STYLE;
+        return;
+      };
+    };
+  }
+  for (let j = 0; j < photo_urls.length; j++) {
+    if (url.href.includes(photo_urls[j])) {
+      news.style.opacity = FILTER_STYLE;
+      return;
+    }
+  };
+};
+
+const checkPress = (news, press) => {
+  let start = news.querySelector("a.api_txt_lines.dsc_txt_wrap").text[0];
+  if (press === '뉴스핌') {
+    if (start === '=') {
+      news.style.opacity = FILTER_STYLE;
+      return;
+    };
+  } else {
+    return;
+  };
+};
+
+const createKwTag = (news, press, title) => {
+  let info = press.replace("언론사 선정", "");
+  let pressKw = `${title.text}(${info}, )`;
+  let copyKw = document.createElement("a");
+  copyKw.innerText = "📋 기사제목(언론사) 복사"
+  copyKw.style = styleDefault;
+  news.querySelector("div.info_group").appendChild(copyKw);
+  copyKw.addEventListener("click", () => {
+    navigator.clipboard.writeText(pressKw)
+  });
+  copyKw.addEventListener("mouseenter", () => {
+    copyKw.style = styleMouseEnter
+  });
+  copyKw.addEventListener("mouseleave", () => {
+    copyKw.style = styleDefault;
+  });
+}
+
+window.onload = () => {
+  let news_arr = document.querySelectorAll("div.news_wrap.api_ani_send");
+  news_arr.forEach((news) => {
+    let press = news.querySelector("a.info.press").text
+    let title = news.querySelector("a.news_tit");
+    let url = news.querySelector("a.news_tit");
+    checkDandok(title);
+    checkArticle(news, title, url);
+    checkPress(news, press);
+    createKwTag(news, press, title);
+  })
+};
+
 const photo_urls = [
   "news1.kr/photos/",
   "newsis.com/view/?id=NISI",
@@ -13,69 +83,14 @@ const del_titles = [
   "장례",
 ];
 
-const checkDandok = (title) => {
-  if (title.text.includes("[단독]")) {
-    title.style = `font-weight: bold;`;
-    title.text = "✅".concat(title.text);
-  }
-};
-
-const checkArticle = (news, title, url) => {
-  const titleHead = title.text.match(/\[[\S|\s]*\]/);
-  if (titleHead) {
-    for (let i = 0; i < del_titles.length; i++) {
-      if (titleHead[0].includes(del_titles[i])) {
-        news.style.opacity = 0.5;
-        return;
-      };
-    };
-  }
-  for (let j = 0; j < photo_urls.length; j++) {
-    if (url.href.includes(photo_urls[j])) {
-      news.style.opacity = 0.5;
-      return;
-    }
-  };
-};
-
-const createKwTag = (news, info, title) => {
-  let pressKw = `${title.text}(${info}, )`;
-  let copyKw = document.createElement("a");
-  copyKw.innerText = "📋 기사제목(언론사) 복사"
-  copyKw.style = `
-    color: #808080;
-    text-decoration: none;
-    padding-left: 0.8rem;
-  `;
-  news.querySelector("div.info_group").appendChild(copyKw);
-  copyKw.addEventListener("click", () => {
-    navigator.clipboard.writeText(pressKw)
-  });
-  copyKw.addEventListener("mouseenter", () => {
-    copyKw.style = `
-      cursor: pointer;
-      color: #808080;
-      text-decoration: underline;
-      padding-left: 0.8rem;
-    `
-  });
-  copyKw.addEventListener("mouseleave", () => {
-    copyKw.style = `
-      color: #808080;
-      text-decoration: none;
-      padding-left: 0.8rem;
-    `
-  });
-}
-
-window.onload = () => {
-  let news_arr = document.querySelectorAll("div.news_wrap.api_ani_send");
-  news_arr.forEach((news) => {
-    let info = news.querySelector("a.info.press").text.replace("언론사 선정", "");
-    let title = news.querySelector("a.news_tit");
-    let url = news.querySelector("a.news_tit");
-    checkDandok(title);
-    checkArticle(news, title, url);
-    createKwTag(news, info, title);
-  })
-};
+const styleDefault = `
+  color: #808080;
+  text-decoration: none;
+  padding-left: 0.8rem;
+`
+const styleMouseEnter = `
+  cursor: pointer;
+  color: #808080;
+  text-decoration: underline;
+  padding-left: 0.8rem;
+`
